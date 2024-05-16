17b854fc674c17e203da8916854120265e1eda25
= <alejandrol@frba.tn.edu.ar>
2024-05-15 23:13:19 -0300

    --

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,18 @@
 BORRAR
  */
 
+
 document.addEventListener("DOMContentLoaded", function () {
-    const formulario = document.getElementById("mi-formulario");
-    const botonBorrar = document.getElementById("boton-borrar");
-  
+  const formulario = document.getElementById("miformulario");
+  const formulario2 = document.getElementById("miformulario2");
+  const botonBorrar = document.getElementById("boton-borrar");
+  const botonEnviar = document.getElementById("boton-enviar");  --
+
     botonBorrar.addEventListener("click", function (event) {
       event.preventDefault();
   
-      formulario.reset(); 
+      miformulario.reset();
+      miformulario2.reset();
     });
   });
 
@@ -18,33 +22,38 @@ document.addEventListener("DOMContentLoaded", function () {
   VALIDACIONES
  */
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const calcularDescuentoButton = document.getElementById("calcular-descuento");
-    const categoriaSelect = document.getElementById("categoria");
-    const cantidadInput = document.getElementById("cantidad");
+  botonEnviar.addEventListener("click", function (event) {
+//    document.addEventListener("DOMContentLoaded", function () {
+    const enviarDatosButton = document.getElementById("enviar");    // "enviar" es el "ID" del HTML
+
     const nombreInput = document.getElementById("nombre");
-    const apellidoInput = document.getElementById("apellido");
-    const emailInput = document.getElementById("email");
-    const precioTotalSpan = document.getElementById("precio-total");
-  
+
+    const fecLlegSelect = document.getElementById("fecLleg");
+    const fecSalSelect = document.getElementById("fecSal");
+
+    const emailInput = document.getElementById("correo");
+    const cantidadInput = document.getElementById("cantidad");
+
     
-    calcularDescuentoButton.addEventListener("click", function (event) {
-        event.preventDefault(); // Evita la recarga de la página por defecto
+    enviarDatosButton.addEventListener("click", function (event) {
+      event.preventDefault(); // Evita la recarga de la página por defecto
   
-        const categoria = categoriaSelect.value;
-        const cantidad = parseInt(cantidadInput.value, 10);
-        const nombre = nombreInput.value;
-        const apellido = apellidoInput.value;
+/*
         const email = emailInput.value;
         const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+*/
+        alert("Por favor, complete todos los campos correctamente.", nombreInput);
 
         if (
-            categoria === "" 
-            ||  isNaN(cantidad)
-            || cantidad <= 0
-            || nombre === ""
-            || apellido === ""
-            || email === "" 
+          nombreInput === "" 
+            || nombreInput === ""
+            || fecLlegSelect === ""
+            || fecSalSelect === ""
+            || emailInput === "" 
+            ||  isNaN(cantidadInput)
+            
+//            ||  isNaN(cantidad)
+//            || cantidad <= 0
 /*
             || emailRegex.match(email) >= 0
 */
@@ -52,27 +61,6 @@ document.addEventListener("DOMContentLoaded", function () {
             alert("Por favor, complete todos los campos correctamente.");
             return;
         }
-  
-        let descuento = 0;
-  
-        switch (categoria) {
-            case "estudiante":
-                descuento = 0.8; // 80% de descuento para Estudiante
-                break;
-            case "trainee":
-                descuento = 0.5; // 50% de descuento para Trainee
-                break;
-            case "junior":
-                descuento = 0.15; // 15% de descuento para Junior
-                break;
-            default:
-                descuento = 0; // Sin descuento por defecto
-        }
-  
-        const precioUnitario = 200; // Precio base por ticket
-        const precioTotal = cantidad * (precioUnitario - (descuento * precioUnitario));
-  
-        precioTotalSpan.textContent = `${precioTotal.toFixed(2)}`;
-    });
+      });
   });
   
